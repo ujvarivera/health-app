3d149a1bbf06cf9d540cd39b1f958ab8dfd417fe
ujvarivera <ujvariveronika0320@gmail.com>
2023-04-19 19:51:48 +0200

    user measurements and goals tables and models added

---
+++ database/migrations/2023_04_19_173105_create_user_goals_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_goals', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('measurement_type_id');
+            $table->unsignedBigInteger('user_id');
+            $table->string('name', 30);
+            $table->float('value');
+            $table->timestamp('completed_at')->nullable();
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('measurement_type_id')->references('id')->on('measurement_types')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_goals');
+    }
+};
