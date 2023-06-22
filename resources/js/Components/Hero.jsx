87ff2a93a315e1c26aa2e878ddc76fdfb9300fe3
ujvarivera <ujvariveronika0320@gmail.com>
2023-06-22 19:10:50 +0200

    updated hero component

--- resources/js/Components/Hero.jsx
+++ resources/js/Components/Hero.jsx
@@ -2,12 +2,18 @@ import ButtonLink from "./ButtonLink";
 
 export default function Hero() {
   return (
-    <div className="bg-purple-400 text-white py-20 h-screen">
-      <div className="container mx-auto flex flex-col items-center justify-center">
-        <img className="w-full max-w-lg mb-8 md:rounded-lg shadow-lg" src="/healthy.jpg" alt="Healthy Image" />
-        <div className="text-center">
-          <h1 className="text-4xl md:text-6xl font-bold mb-4">Welcome to Our Healthy Website</h1>
-          <p className="text-lg mb-8">Discover the benefits of a healthy lifestyle.</p>
+    <div className="relative text-white min-h-screen">
+      <div className="absolute inset-0">
+        <img className="w-full h-full object-cover" src="/healthy.jpg" alt="Healthy Image" />
+      </div>
+      <div className="absolute inset-0 flex flex-col items-center justify-center">
+        <div className="container mx-auto text-center">
+          <h1 className="text-4xl md:text-6xl font-bold mb-4 bg-white bg-opacity-70 text-purple-700 p-4 shadow-m">
+            Welcome to Our Healthy Website
+          </h1>
+          <p className="text-lg font-bold mb-8 bg-white bg-opacity-70 text-purple-700 p-4 shadow-m">
+            Discover the benefits of a healthy lifestyle.
+          </p>
           <div className="flex justify-center">
             <ButtonLink href={route('login')} className="w-40 mx-2 flex flex-col items-center">LOGIN</ButtonLink>
             <ButtonLink href={route('register')} className="w-40 flex flex-col items-center">REGISTER</ButtonLink>
